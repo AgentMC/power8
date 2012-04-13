cd65a8b08e33deec34eebb95aa2b386a0889a57c
agentmc@mail.ru <agentmc@mail.ru@085e4529-62bf-09fd-2e3b-1b06052ae640>
2012-04-13 14:37:16 +0000

    Added missed files

---
+++ MenuItemClickCommand.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Windows.Input;
+
+namespace Power8
+{
+    public class MenuItemClickCommand : ICommand
+    {
+        public void Execute(object parameter)
+        {
+            var powerItem = parameter as PowerItem;
+            if (powerItem == null || powerItem.Parent == null)
+                return;
+            powerItem.Invoke();
+            BtnStck.Instance.Hide();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ MenuedButton.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Power8
+{
+    /// <summary>
+    /// Interaction logic for MenuedButton.xaml
+    /// </summary>
+    public partial class MenuedButton:INotifyPropertyChanged
+    {
+        public MenuedButton()
+        {
+            InitializeComponent();
+            ItemsList.DataContext = this;
+            ItemsList.PlacementTarget = menuDropper;
+            ItemsList.Placement = PlacementMode.Left;
+        }
+
+        private PowerItem _item;
+        public PowerItem Item
+        {
+            get { return _item; }
+            set
+            {
+                if (_item != value)
+                {
+                    _item = value;
+                    OnPropertyChanged("Item");
+                }
+            }
+        }
+
+        private ContextMenu ItemsList
+        {
+            get { return ((ContextMenu) Resources["itemsList"]); }
+        }
+
+        private void DisplayMenu(object sender, RoutedEventArgs e)
+        {
+            ItemsList.IsOpen = true;
+        }
+
+        public BtnStck Mnu
+        {
+            get { return BtnStck.Instance; }
+        }
+
+        private void ContextMenuContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+            ((App)Application.Current).MenuDataContext = Util.ExtractRelatedPowerItem(e);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string property)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
