5b0e476d2165da26d98d30cbbe343ff82e67381b
agentmc@mail.ru <agentmc@mail.ru@085e4529-62bf-09fd-2e3b-1b06052ae640>
2012-04-25 08:39:25 +0000

    Added missed file

---
+++ trunk/NameToResourceConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Power8
+{
+    [ValueConversion(typeof(string), typeof(string))]
+    [TypeConverter(typeof(string))]
+    class NameToResourceConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Properties.Resources.ResourceManager.GetString("CR_" + (string) value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
