76b9b63a0db823947950bd46afeef4f39478bd1f
agentmc@mail.ru <agentmc@mail.ru@085e4529-62bf-09fd-2e3b-1b06052ae640>
2012-05-25 10:57:19 +0000

    Fixed bindings for UpdateNotifier Added dynamic margin for menu template

---
+++ MenuWidthToMarginConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+#if DEBUG
+using System.Diagnostics;
+#endif
+
+namespace Power8
+{
+    [ValueConversion(typeof(double), typeof(Thickness))]
+    [TypeConverter(typeof(double))]
+    class MenuWidthToMarginConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int dbl = (int) SystemParameters.MenuWidth;
+            if (dbl == 19) //Aero/7
+                return new Thickness(-30, 0, 0, 0);
+            if (dbl == 18) //Classic/7
+                return new Thickness(-14, 2, 0, 2);
+#if DEBUG
+            Debug.WriteLine("dbl="+dbl);
+#endif
+            return new Thickness(0, 0, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
