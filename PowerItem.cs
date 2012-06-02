378f9572b4472c5a26713ad12e23151eaa89b9b5
agentmc@mail.ru <agentmc@mail.ru@085e4529-62bf-09fd-2e3b-1b06052ae640>
2012-06-02 22:19:58 +0000

    Internal search almost done todo: esc handling, Match() enhancements

--- PowerItem.cs
+++ PowerItem.cs
@@ -236,7 +236,7 @@ namespace Power8
 
         public override int GetHashCode()
         {
-            return (FriendlyName + IsFolder).GetHashCode();
+            return (FriendlyName + IsFolder + Argument).GetHashCode();
         }
 
         public override bool Equals(object obj)
