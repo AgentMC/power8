6923ec15aeac6fed6dd74be19ac4bf62062b3631
agentmc@mail.ru <agentmc@mail.ru@085e4529-62bf-09fd-2e3b-1b06052ae640>
2012-06-05 21:44:26 +0000

    Changed width converter to support release preview

--- MenuWidthToMarginConverter.cs
+++ MenuWidthToMarginConverter.cs
@@ -17,9 +17,15 @@ namespace Power8
         {
             int dbl = (int) SystemParameters.MenuWidth;
             if (dbl == 19) //Aero/7
+            {
+                if(Environment.OSVersion.Version >= new Version(6,2,0,8400))
+                    return new Thickness(-42, 0, 0, 0);
                 return new Thickness(-30, 0, 0, 0);
+            }
             if (dbl == 18) //Classic/7
-                return new Thickness(-14, 2, 0, 2);
+            { 
+                return new Thickness(-14, 2, 0, 2); 
+            }
 #if DEBUG
             Debug.WriteLine("dbl="+dbl);
 #endif
