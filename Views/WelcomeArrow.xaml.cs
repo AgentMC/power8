e20f1b2d01d1dbc14efad556c3002151de23c434
agentmc@mail.ru <agentmc@mail.ru@085e4529-62bf-09fd-2e3b-1b06052ae640>
2012-06-11 14:36:58 +0000

    Preparations to welcome Arrow...

---
+++ Views/WelcomeArrow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace Power8.Views
+{
+    public partial class WelcomeArrow : INotifyPropertyChanged
+    {
+        public WelcomeArrow()
+        {
+            InitializeComponent();
+        }
+
+        private double _rotation;
+        public double Rotation 
+        {
+            get { return _rotation; } 
+            set
+            {
+                _rotation = value;
+                var h = PropertyChanged;
+                if (h != null)
+                    h(this, new PropertyChangedEventArgs("Rotation"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
