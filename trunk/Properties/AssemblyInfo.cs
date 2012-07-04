7ce2eb340e60d167907a5deb37374af00695c6b2
agentmc@mail.ru <agentmc@mail.ru@085e4529-62bf-09fd-2e3b-1b06052ae640>
2012-07-04 08:54:30 +0000

    ==NEW VERSION!== Beta 1: 0.5.0.0 MSI engine: 9.3

--- trunk/Properties/AssemblyInfo.cs
+++ trunk/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
-//7zuri=http://power8.googlecode.com/files/Power8_v.0.4.0.0.7z
-//msuri=http://power8.googlecode.com/files/Power8_v.0.4.0.0.msi
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
+//7zuri=http://power8.googlecode.com/files/Power8_v.0.5.0.0.7z
+//msuri=http://power8.googlecode.com/files/Power8_v.0.5.0.0.msi
