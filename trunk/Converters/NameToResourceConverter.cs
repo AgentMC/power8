caaf229e04c7cc4d35c065afc92c290e4694491c
agentmc@mail.ru <agentmc@mail.ru@085e4529-62bf-09fd-2e3b-1b06052ae640>
2012-08-28 11:33:25 +0000

    Remove ResX-style localization for RestartExplorer NameToResourceConverter.cs: added overload and better documentation Installer: added IT

--- trunk/Converters/NameToResourceConverter.cs
+++ trunk/Converters/NameToResourceConverter.cs
@@ -15,11 +15,28 @@ namespace Power8.Converters
     [TypeConverter(typeof(string))]
     class NameToResourceConverter:IValueConverter
     {
+        /// <summary>
+        /// Converts the control name given into a localized control text.
+        /// </summary>
+        /// <param name="value">String, the name of control. "CR_" is added to it and is searched in resources.</param>
+        /// <param name="targetType">Not used</param>
+        /// <param name="parameter">Not used</param>
+        /// <param name="culture">Not used</param>
+        /// <returns>String to be put into control Content or Text or similar property</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return Properties.Resources.ResourceManager.GetString("CR_" + (string) value);
         }
 
+        /// <summary> Shortcut to be used from the code </summary>
+        /// <param name="controlName">String, the name of control. "CR_" is added to it and is searched in resources.</param>
+        /// <returns>String to be put into control Content or Text or similar property</returns>
+        public string Convert(string controlName)
+        {
+            return (string) Convert(controlName, null, null, null);
+        }
+
+        /// <summary> Not implemented </summary>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
