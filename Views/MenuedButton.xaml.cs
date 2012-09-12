c4a438c5d6247c5a83d737976fc80aa68c8def02
agentmc@mail.ru <agentmc@mail.ru@085e4529-62bf-09fd-2e3b-1b06052ae640>
2012-09-12 21:45:39 +0000

    Documented and refactorewd: App, Menued button, RE. Left: MainWindow, PowerItem.

--- Views/MenuedButton.xaml.cs
+++ Views/MenuedButton.xaml.cs
@@ -4,7 +4,7 @@ using System.Windows.Controls;
 namespace Power8
 {
     /// <summary>
-    /// Interaction logic for MenuedButton.xaml
+    /// MenuedButton the button with image-dropdown. Represents PowerItem.
     /// </summary>
     public partial class MenuedButton:INotifyPropertyChanged
     {
@@ -14,30 +14,36 @@ namespace Power8
         }
 
         private PowerItem _item;
+        /// <summary>
+        /// The PowerItem represented by this menued button.
+        /// This is not implemented as DependencyProperty since a lot of work 
+        /// is done automatically in case of DP, so the designer can crash 
+        /// from time to time. This doesn't happen with regular property.
+        /// </summary>
         public PowerItem Item
         {
             get { return _item; }
             set
             {
-                if (_item != value)
-                {
-                    _item = value;
-                    OnPropertyChanged("Item");
-                }
+                if (_item == value) 
+                    return;
+                _item = value;
+                var handler = PropertyChanged;
+                if (handler != null) 
+                    handler(this, new PropertyChangedEventArgs("Item"));
             }
         }
 
+        /// <summary>
+        /// The context menu is over the drop-down PowerItems.
+        /// So what we are doing is setting the data context for that menu 
+        /// to the PowerItem clicked.
+        /// </summary>
         private void ContextMenuContextMenuOpening(object sender, ContextMenuEventArgs e)
         {
             App.Current.MenuDataContext = Util.ExtractRelatedPowerItem(e);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-
-        public void OnPropertyChanged(string property)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null) handler(this, new PropertyChangedEventArgs(property));
-        }
     }
 }
--- Views/RestartExplorer.cs
+++ Views/RestartExplorer.cs
@@ -3,8 +3,19 @@ using Power8.Converters;
 
 namespace Power8.Views
 {
+    /// <summary>
+    /// The win32 window that is shown in case Explorer is dead 
+    /// and we got to do something. Contains no logic except the localization.
+    /// The only function of this is the question to user:
+    /// Do you know what's happening or not? Because we don't like it!
+    /// So the only thing whis window does is to return Yes or No :)
+    /// </summary>
     public partial class RestartExplorer : Form
     {
+        /// <summary>
+        /// Constructor. Performs form init and loads the 
+        /// localized strings for current UI culture.
+        /// </summary>
         public RestartExplorer()
         {
             InitializeComponent();
@@ -14,7 +25,10 @@ namespace Power8.Views
             button1.Text = c.Convert("RE_YesButton");
             button2.Text = c.Convert("RE_NoButton");
   }
-
+        /// <summary>
+        /// ShowDialog() is overriden to guarantee the window will be 
+        /// displayed foreground on all OS in the same way.
+        /// </summary>
         new public void ShowDialog()
         {
             Show();
