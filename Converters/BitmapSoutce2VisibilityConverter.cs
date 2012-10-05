d6210f67d896d0b3bee6ee7b40104edbda920ce5
agentmc@mail.ru <agentmc@mail.ru@085e4529-62bf-09fd-2e3b-1b06052ae640>
2012-10-05 20:49:06 +0000

    Added missed file

---
+++ Converters/BitmapSoutce2VisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Power8.Converters
+{
+    /// <summary>
+    ///
+    /// </summary>
+    [ValueConversion(typeof(BitmapSource), typeof(Visibility))]
+    [TypeConverter(typeof(BitmapSource))]
+    class BitmapSource2VisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (((string)parameter) == "true")
+                return value == null ? Visibility.Hidden : Visibility.Visible;
+            return value == null ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
