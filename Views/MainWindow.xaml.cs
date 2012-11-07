7b50079ddb26f644293fc3b4a0ba45ee3d2a94ed
agentmc@mail.ru <agentmc@mail.ru@085e4529-62bf-09fd-2e3b-1b06052ae640>
2012-11-07 22:40:14 +0000

    Implemented restoration of taskbar's rebar after application exit. Previously Update window thread might start up earlier than Main window is actually placed in Taskbar. Fixed. Simplified MainWindow XAML a bit Documented MainWindows cs

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -15,25 +15,29 @@ using Application = System.Windows.Forms.Application;
 namespace Power8.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// MainWindow - a Start Button of Power8. This class is the main one.
     /// </summary>
     public partial class MainWindow:INotifyPropertyChanged
     {
-        public static bool ClosedW;
-
+        public static bool ClosedW; //Indicates that App is going to init the shutdown soon...
+        //Both used to set proper placement for Context Menu over Main Button
         private static readonly Window PlacementWnd = new Window { Width = 10, Height = 10 };
         private static readonly Point PlacementPoint = new Point(0, 0);
         
         public event PropertyChangedEventHandler PropertyChanged;
 
-        private IntPtr _taskBar, _showDesktopBtn, _midPanel;
+        private IntPtr _taskBar, _showDesktopBtn, _midPanel; //system windows handles
         private WelcomeArrow _arrow;
-        private PlacementMode _placement = PlacementMode.MousePoint;
-        private BitmapSource _bitmap;
-        private string _lastSource;
+        private PlacementMode _placement = PlacementMode.MousePoint; //used for context menu
+        private BitmapSource _bitmap; //picture on the Main Button
+        private string _lastSource;   //file path of _bitmap
 
         #region Window (de)init 
 
+        /// <summary>
+        /// Constructor. Initializes layout of window, sets datacontext, switches on theming on 
+        /// WinForms dialogs, and so on.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
@@ -53,7 +57,11 @@ namespace Power8.Views
             }
                 
         }
-
+        /// <summary>
+        /// Handles event that occurs when ButtonStack instance was generated. This happens on startup,
+        /// so is a part of initialization of MainWindow. 
+        /// Changes cursor and shows welcome arrow if required.
+        /// </summary>
         void BtnStckInstanciated(object sender, EventArgs e)
         {
             BtnStck.Instance.RunCalled += ShowRunDialog;
@@ -72,8 +80,8 @@ namespace Power8.Views
                 _arrow.Rotation = p1.Y < p2.Y ? -initialAngle : initialAngle;
             }
         }
-
 // ReSharper disable RedundantAssignment
+        /// <summary> Hooks WndProc to react on HOTKEY passed (Alt+Z) </summary>
         private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
         {
             handled = false;
@@ -88,7 +96,10 @@ namespace Power8.Views
             return IntPtr.Zero;
         }
 // ReSharper restore RedundantAssignment
-
+        /// <summary>
+        /// Locates Windows system windows, like TaskBar and locates Power8 MainButton inside it.
+        /// Starts location update thread. Subscribes to Settings Manager events and registers HotKey.
+        /// </summary>
         private void WindowLoaded(object sender, RoutedEventArgs e)
         {
             _taskBar = API.FindWindow(API.WndIds.TRAY_WND_CLASS, null);
@@ -110,18 +121,21 @@ namespace Power8.Views
 
             Left = 0;
             Top = 0;
+            API.SetParent(this.MakeGlassWpfWindow(), _taskBar);
             Util.Fork(WatchDesktopBtn, "ShowDesktop button watcher").Start();
-
+            
             SettingsManager.WarnMayHaveChanged += SettingsManagerOnWarnMayHaveChanged;
             SettingsManager.ImageChanged += SettingsManagerOnImageChanged;
             SettingsManager.BgrThreadLock.Set();
 
-            API.SetParent(this.MakeGlassWpfWindow(), _taskBar);
-
             API.RegisterHotKey(this.GetHandle(), 0, API.fsModifiers.MOD_ALT, Keys.Z);
             this.RegisterHook(WndProc);
         }
-
+        /// <summary>
+        /// Sync point when closing the application. If the main window was closed by Alt+F4,
+        /// by killing Explorer, or by shutdown of the system - controlls that still all the 
+        /// threads will exit by invoking Main dispatcher shutdown.
+        /// </summary>
         private void WindowClosed(object sender, EventArgs e)
         {
             ClosedW = true;
@@ -135,6 +149,11 @@ namespace Power8.Views
 
         #region Handlers
 
+        /// <summary>
+        /// Handles click on the Main button. By default shows/hides Buttonstack, 
+        /// with CTRL pressed shows Run dialog instead, and with CTRL+SHIFT in DEBUG
+        /// mode shows RestartExplorer instance for purposes of localization
+        /// </summary>
         private void ShowButtonStack(object sender, RoutedEventArgs e)
         {
             if(!BtnStck.IsInstantited)
@@ -161,28 +180,45 @@ namespace Power8.Views
             API.GetCursorPos(ref screenPoint);
             GetSetWndPosition(BtnStck.Instance, screenPoint, sender == Keyboard.PrimaryDevice);
         }
-
+        /// <summary>
+        /// Shows Run dialog
+        /// </summary>
         private void ShowRunDialog(object sender, EventArgs e)
         {
             API.SHRunDialog(IntPtr.Zero, IntPtr.Zero, null, null, null, API.RFF.NORMAL);
         }
-
+        /// <summary>
+        /// Closes Main window exiting application
+        /// </summary>
         private void ExitClick(object sender, RoutedEventArgs e)
         {
             Close();
             SettingsManager.BgrThreadLock.WaitOne();
         }
-
+        /// <summary>
+        /// Shows About dialog
+        /// </summary>
         private void AboutClick(object sender, RoutedEventArgs e)
         {
             Util.InstanciateClass(t: typeof (About));
         }
-
+        /// <summary>
+        /// Shows Settings dialog
+        /// </summary>
+        private void SettingsClick(object sender, RoutedEventArgs e)
+        {
+            Util.InstanciateClass(t: typeof(SettingsWnd));
+        }
+        /// <summary>
+        /// Hides WelcomeArrow when mouse howers Main button
+        /// </summary>
         private void MainBtnMouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
             KillArrow();
         }
-
+        /// <summary>
+        /// Fixes placement of Main Button's Context Menu depending on Taskbar layout
+        /// </summary>
         private void MainBtnLayoutUpdated(object sender, EventArgs e)
         {
             if (!IsLoaded || Util.OsIs.XPOrLess)
@@ -194,18 +230,19 @@ namespace Power8.Views
             else                         //Taskbar horizontal
                 ContextPlacement = PlacementWnd.Top > p.Y ? PlacementMode.Bottom : PlacementMode.Top;
         }
-
-        private void ShowSettingsWindow(object sender, RoutedEventArgs e)
-        {
-            Util.InstanciateClass(t: typeof(SettingsWnd));
-        }
-
+        /// <summary>
+        /// Reacts on the event indicating that settings state may had changed, so warning 
+        /// icon and text must be displayed or hidden
+        /// </summary>
         private void SettingsManagerOnWarnMayHaveChanged(object sender, EventArgs eventArgs)
         {
             FirePropChanged("Tip");
             FirePropChanged("WarningIconVisibility");
         }
-
+        /// <summary>
+        /// Invokes PropertyChanged for StartImage property making UI react on user changing 
+        /// the picture in settings
+        /// </summary>
         private void SettingsManagerOnImageChanged(object sender, EventArgs eventArgs)
         {
             FirePropChanged("StartImage");
@@ -215,13 +252,15 @@ namespace Power8.Views
 
         #region Background threads
 
+        /// <summary>
+        /// Thread watches Taskbar and configures layout of Main button accordingly
+        /// </summary>
         private void WatchDesktopBtn()
         {
             double width = -1, height = -1;
-
+            API.RECT r;
             while (!ClosedW)
             {
-                API.RECT r;
                 if (!API.GetWindowRect(_showDesktopBtn, out r))
                 {//looks like explorer.exe is dead!
                     Thread.Sleep(10000);//let's wait for explorer to auto-restart
@@ -255,10 +294,11 @@ namespace Power8.Views
                 //Size the button by ShowDesktop buton
                 var curHeight = r.Bottom - r.Top;
                 var curWidth = r.Right - r.Left;
-                bool checkTop = curHeight < curWidth;
+                bool taskBarVertical = curHeight < curWidth;
+
                 if (SettingsManager.Instance.SquareStartButton)
                 {//Apply user size modification
-                    var sizeCoef = SettingsManager.GetARModifier(!checkTop);
+                    var sizeCoef = SettingsManager.GetARModifier(!taskBarVertical);
                     if (curHeight > curWidth) //vertical button, horiz. bar
                         curWidth = (int)(curHeight * sizeCoef);
                     else                      //horiz. button, vertical bar
@@ -274,35 +314,56 @@ namespace Power8.Views
                     height = curHeight;
                     Dispatcher.Invoke(new Action(() =>  b1.Height = curHeight));
                 }
+
                 //If required, apply move to taskbar rebar
                 if (Util.OsIs.EightOrMore || (SettingsManager.Instance.SquareStartButton && Util.OsIs.SevenOrMore))
-                {
-                    API.RECT r2;
-                    API.GetWindowRect(_midPanel, out r);
-                    API.GetWindowRect(_taskBar, out r2);
-                    if (Util.OsIs.SevenOrBelow && !API.DwmIsCompositionEnabled())
-                    { //Have no idea why, but there's some kind of automatic margin applied in classic style
-                        if (checkTop)
-                            r2.Left += 4;
-                        else
-                            r2.Top += 4;
-                    }
-                    r.Top -= r2.Top;
-                    r.Left -= r2.Left;
-                    r.Right -= r2.Left;
-                    r.Bottom -= r2.Top;
-                    if (checkTop && r.Top + 4 != curHeight)
-                    {//move rebar down
-                        int delta = (curHeight - 4) - r.Top;
-                        API.MoveWindow(_midPanel, r.Left, r.Top + delta, r.Right - r.Left, r.Bottom - r.Top - delta, true);
-                    }
-                    else if (!checkTop && r.Left + 4 != curWidth)
-                    {//move rebar right
-                        int delta = (curWidth - 4) - r.Left;
-                        API.MoveWindow(_midPanel, r.Left + delta, r.Top, r.Right - r.Left - delta, r.Bottom - r.Top, true);
-                    }
-                }
+                    MoveReBar(taskBarVertical, curHeight, curWidth);
+
                 Thread.Sleep(100);
+            }
+            //restoring taskbar on exit
+            if (Util.OsIs.EightOrMore && API.GetWindowRect(_showDesktopBtn, out r))
+            {//free space to the left/up is the same as in right/down, which is show desktop btn
+                var curHeight = r.Bottom - r.Top;
+                var curWidth = r.Right - r.Left;
+                MoveReBar(curHeight < curWidth, curHeight, curWidth);
+            }
+        }
+
+        /// <summary>
+        /// Moves "rebar" panel that hosts applications' buttons and different launch bars on taskbar
+        /// </summary>
+        /// <param name="taskBarVertical">Is taskbar located vertical or horizontal</param>
+        /// <param name="curHeight">Desired Height of the gap to the left/top of app buttons</param>
+        /// <param name="curWidth">Desired Width of the gap to the left/top of app buttons</param>
+        private void MoveReBar(bool taskBarVertical, int curHeight, int curWidth)
+        {
+            API.RECT r, r2;
+            API.GetWindowRect(_midPanel, out r); //absolute coord of rebar
+            API.GetWindowRect(_taskBar, out r2); //absolute coord of taskbar
+            if (Util.OsIs.SevenOrBelow && !API.DwmIsCompositionEnabled())
+            {//This if doesn't work on exit since it is called only for Win8 on exit
+                //Have no idea why, but there's some kind of automatic margin applied in classic style
+                if (taskBarVertical)
+                    r2.Left += 4;
+                else
+                    r2.Top += 4;
+            }
+            r.Top -= r2.Top; //getting relative coordinates...
+            r.Left -= r2.Left;
+            r.Right -= r2.Left;
+            r.Bottom -= r2.Top;
+            if (taskBarVertical && r.Top + 4 != curHeight) //start moving!
+            {
+                //move rebar down (up on exit)
+                int delta = (curHeight - 4) - r.Top;
+                API.MoveWindow(_midPanel, r.Left, r.Top + delta, r.Right - r.Left, r.Bottom - r.Top - delta, true);
+            }
+            else if (!taskBarVertical && r.Left + 4 != curWidth)
+            {
+                //move rebar right (left on exit)
+                int delta = (curWidth - 4) - r.Left;
+                API.MoveWindow(_midPanel, r.Left + delta, r.Top, r.Right - r.Left - delta, r.Bottom - r.Top, true);
             }
         }
 
@@ -310,6 +371,10 @@ namespace Power8.Views
 
         #region Bindable props
 
+        /// <summary>
+        /// Returns currently desired placement of MainButton Context menu
+        /// in telation to this button
+        /// </summary>
         public PlacementMode ContextPlacement
         {
             get { return _placement; }
@@ -321,7 +386,9 @@ namespace Power8.Views
                 FirePropChanged("ContextPlacement");
             }
         }
-
+        /// <summary>
+        /// Returns tooltip that must be shown currently over the Main Button
+        /// </summary>
         public string Tip
         {
             get
@@ -332,7 +399,9 @@ namespace Power8.Views
                             : string.Empty);
             }
         }
-
+        /// <summary>
+        /// Returns value indicating should warning icon be displayed at the moment
+        /// </summary>
         public Visibility WarningIconVisibility
         {
             get
@@ -340,7 +409,9 @@ namespace Power8.Views
                 return SettingsManager.Instance.ShowWarn ? Visibility.Visible : Visibility.Hidden;
             }
         }
-
+        /// <summary>
+        /// Returns BitmapSource containing user-chosen picture placed onto MainButton
+        /// </summary>
         public BitmapSource StartImage
         {
             get
@@ -373,12 +444,30 @@ namespace Power8.Views
 
         #region Helpers
 // ReSharper disable UnusedParameter.Local
+        /// <summary>
+        /// Kills application if passed window handle is NULL with coresponding message.
+        /// </summary>
+        /// <param name="wnd">Window handle to be checked</param>
+        /// <param name="className">Class name of window (or any other identifier). 
+        /// Used for logging purposes.</param>
         private static void CheckWnd(IntPtr wnd, string className)
         {
             if (wnd == IntPtr.Zero)
                 Util.Die(className + " not found");
         }
 // ReSharper restore UnusedParameter.Local
+        /// <summary>
+        /// Locates the window passed as if it is a Progman, the Start Menu.
+        /// First, by screenPoint passed calculates active screen.
+        /// By it's bounds determines the location of TaskBar.
+        /// Finally, calculates the position of target window.
+        /// Shows window except if it is a Placement rectangle.
+        /// Focuses window if it is a ButtonStack.
+        /// </summary>
+        /// <param name="w">Window to locate</param>
+        /// <param name="screenPoint">Unmanaged struct containing absolute screen coordinates, in px.</param>
+        /// <param name="ignoreTaskbarPosition">Flag instructing method to ignore the location of task bar
+        /// locating w simply in the corner closest to the screenPoint. E.g. to show BtnStch by Alt+Z.</param>
         private static void GetSetWndPosition(Window w, API.POINT screenPoint, bool ignoreTaskbarPosition)
         {
             var resPoint = new Point();
@@ -407,13 +496,17 @@ namespace Power8.Views
 
             w.Left = resPoint.X;
             w.Top = resPoint.Y;
-            if((int)w.Height != 10)
+// ReSharper disable PossibleUnintendedReferenceComparison
+            if(w != PlacementWnd)
                 w.Activate();
+// ReSharper restore PossibleUnintendedReferenceComparison
             var b = w as BtnStck;
             if (b != null)
-                b.Focus();
+                b.Focus();//Focus() is __new on ButtonStack, must explicitly type
         }
-
+        /// <summary>
+        /// Hides WelcomeArrow if it is available
+        /// </summary>
         private void KillArrow()
         {
             if (_arrow == null) 
@@ -421,7 +514,10 @@ namespace Power8.Views
             _arrow.Close();
             _arrow = null;
         }
-
+        /// <summary>
+        /// Invokes PropertyChanged event
+        /// </summary>
+        /// <param name="propName">Name of the property that had changed</param>
         private void FirePropChanged(string propName)
         {
             var h = PropertyChanged;
