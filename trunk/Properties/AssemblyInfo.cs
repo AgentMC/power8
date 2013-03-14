a3521c2206d95835e0fdbe1fa8e063c84e396050
agentmc@mail.ru <agentmc@mail.ru@085e4529-62bf-09fd-2e3b-1b06052ae640>
2013-03-14 08:06:51 +0000

    ===NEW VERSION 1.4.1===

--- trunk/Properties/AssemblyInfo.cs
+++ trunk/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Power8 Team")]
 [assembly: AssemblyProduct("Power8")]
-[assembly: AssemblyCopyright("© Power8 Team 2012")]
+[assembly: AssemblyCopyright("© Power8 Team 2012-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.555")]
-[assembly: AssemblyFileVersion("1.4.0.555")]
-//7zuri=http://power8.googlecode.com/files/Power8_v.1.4.0.555.7z
-//msuri=http://power8.googlecode.com/files/Power8_v.1.4.0.555.msi
+[assembly: AssemblyVersion("1.4.1.569")]
+[assembly: AssemblyFileVersion("1.4.1.569")]
+//7zuri=http://power8.googlecode.com/files/Power8_v.1.4.1.569.7z
+//msuri=http://power8.googlecode.com/files/Power8_v.1.4.1.569.msi
