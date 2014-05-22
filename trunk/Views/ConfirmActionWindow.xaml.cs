548867a7d992c3a2f62b64b66df6d40dce2eb327
xabalevskii@gmail.com <xabalevskii@gmail.com@085e4529-62bf-09fd-2e3b-1b06052ae640>
2014-05-22 08:36:13 +0000

    - Using SystemIcons.Question icon instead of question mark in ConfirmActionWindow - ConfirmActionWindow Layout updated

--- trunk/Views/ConfirmActionWindow.xaml.cs
+++ trunk/Views/ConfirmActionWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using System.Drawing;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using Power8.Properties;
 
 namespace Power8.Views
@@ -11,8 +17,17 @@ namespace Power8.Views
         public ConfirmActionWindow(object action)
         {
             InitializeComponent();
+
+            QuestionIcon.Source = GetQuestionIconSource();
             Title = NoLoc.Stg_AppShortName;
             CawConfirmButton.Content = action;
+        }
+
+        private static BitmapSource GetQuestionIconSource()
+        {
+            var icon = SystemIcons.Question;
+            var bs = Imaging.CreateBitmapSourceFromHIcon(icon.Handle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            return bs;
         }
 
         public ConfirmActionWindow(string confirmMessage, string confirmButtonText, string cancelButtonText)
