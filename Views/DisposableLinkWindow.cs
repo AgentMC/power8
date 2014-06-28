3eaee9e5753b4440cfc4089f099e8917e732e1c0
agentmc@mail.ru <agentmc@mail.ru@085e4529-62bf-09fd-2e3b-1b06052ae640>
2014-06-28 23:27:35 +0000

    Optimized Usings Optimized code for launching web links from UI Corrected messages a bit

---
+++ Views/DisposableLinkWindow.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+
+namespace Power8.Views
+{
+    public class DisposableLinkWindow: DisposableWindow
+    {
+        /// <summary>
+        /// Handles clicks on the Hyperlinks in this window
+        /// </summary>
+        protected void Navigate(object sender, RoutedEventArgs e)
+        {
+            Util.CreateProcess(((Hyperlink)sender).NavigateUri.AbsoluteUri);
+        }
+    }
+}
