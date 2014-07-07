280e57d4a4f78bce0982a541483da42711dc3c25
agentmc@mail.ru <agentmc@mail.ru@085e4529-62bf-09fd-2e3b-1b06052ae640>
2014-07-07 21:02:03 +0000

    ===NEW VERSION 1.5.2.777=== Loc 1.5.2: CS Left: AR, DE, ES, FR, IT, NL, TR, ZH-HANS, ZH-TW

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -49,7 +49,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.1.766")]
-[assembly: AssemblyFileVersion("1.5.1.766")]
-//7zuri=http://power8.googlecode.com/svn/filez/Power8_v.1.5.1.766.7z
-//msuri=http://power8.googlecode.com/svn/filez/Power8_v.1.5.1.766.msi
+[assembly: AssemblyVersion("1.5.2.777")]
+[assembly: AssemblyFileVersion("1.5.2.777")]
+//7zuri=http://power8.googlecode.com/svn/filez/Power8_v.1.5.2.777.7z
+//msuri=http://power8.googlecode.com/svn/filez/Power8_v.1.5.2.777.msi
